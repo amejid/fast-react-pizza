b42d0ea9294c5da06258a0aa83d1165d9d6ed8a7
amejid <abdelmejid89@gmail.com>
2023-09-04 12:09:25 +0300

    Style cart

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,30 @@
+import { formatCurrency } from '../../utils/helpers.js';
+import PropTypes from 'prop-types';
+import Button from '../../ui/Button.jsx';
+
+const CartItem = ({ item }) => {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity}&times; {name}
+      </p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
+      </div>
+    </li>
+  );
+};
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    pizzaId: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+  }),
+};
+
+export default CartItem;
